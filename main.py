9955ffe8dd6f3c8629ccbe277ab6cc0f1cf1b9db
Henry Huang <keenhenry1109@gmail.com>
2011-04-27 21:46:59 +0800

    Added two CGI scripts to process main page loading and the action of uploading files

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import cgi
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import users
+from google.appengine.ext import db
+
+class CSVFile(db.Model):
+    author = db.UserProperty()
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        
+	template_values = {
+            #'csvfiles': csvfiles
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))	
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
